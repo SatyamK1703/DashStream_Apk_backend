888619e641c26cf34e4b4e6dd01c43e07011fb12
Azlan Ehtasham <azlanehtesham02@gmail.com>
2025-10-12 20:36:05 +0530

    fix

--- src/config/razorpay.js
+++ src/config/razorpay.js
@@ -4,6 +4,9 @@ const keyId = process.env.RAZORPAY_KEY_ID || "";
 const keySecret = process.env.RAZORPAY_KEY_SECRET || "";
 const webhookSecret = process.env.RAZORPAY_WEBHOOK_SECRET || "";
 
+console.log('Razorpay Key ID:', keyId);
+console.log('Razorpay Key Secret:', keySecret);
+
 const instance = new Razorpay({
   key_id: keyId,
   key_secret: keySecret,
