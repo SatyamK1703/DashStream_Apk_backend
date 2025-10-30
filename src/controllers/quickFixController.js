3d476a1d5023693c71c17ac365437a63a846aaeb
Azlan Ehtasham <azlanehtesham02@gmail.com>
2025-10-31 00:08:50 +0530

    minor fix

--- src/controllers/quickFixController.js
+++ src/controllers/quickFixController.js
@@ -13,11 +13,12 @@ const getQuickFixes = asyncHandler(async (req, res) => {
 // @route   POST /api/quick-fixes
 // @access  Private/Admin
 const createQuickFix = asyncHandler(async (req, res) => {
-  const { label, image } = req.body;
+  const { label, image, isActive } = req.body;
 
   const quickFix = new QuickFix({
     label,
     image,
+    isActive,
   });
 
   const createdQuickFix = await quickFix.save();
@@ -28,13 +29,16 @@ const createQuickFix = asyncHandler(async (req, res) => {
 // @route   PUT /api/quick-fixes/:id
 // @access  Private/Admin
 const updateQuickFix = asyncHandler(async (req, res) => {
-  const { label, image } = req.body;
+  const { label, image, isActive } = req.body;
 
   const quickFix = await QuickFix.findById(req.params.id);
 
   if (quickFix) {
     quickFix.label = label || quickFix.label;
     quickFix.image = image || quickFix.image;
+    if (isActive !== undefined) {
+      quickFix.isActive = isActive;
+    }
 
     const updatedQuickFix = await quickFix.save();
     res.json(updatedQuickFix);
@@ -51,7 +55,7 @@ const deleteQuickFix = asyncHandler(async (req, res) => {
   const quickFix = await QuickFix.findById(req.params.id);
 
   if (quickFix) {
-    await quickFix.remove();
+    await QuickFix.deleteOne({ _id: req.params.id });
     res.json({ message: 'Quick fix removed' });
   } else {
     res.status(404);
--- src/models/QuickFix.js
+++ src/models/QuickFix.js
@@ -9,6 +9,10 @@ const QuickFixSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
   // You can add more fields here as needed, for example:
   // description: String,
   // price: Number,
